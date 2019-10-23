53f9325733c8cdba4b0c015ccebe873ebe406110
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-23 10:06:51 -0500

    Computed properties

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,8 +1,17 @@
 <template lang="pug">
   #app
+    label(for="name") {{ 'Nombre' }}
     input(v-model="name")
-    p {{ name }}
-    a(:href="url") Link
+    label(for="lastname") {{ 'Apellido' }}
+    input(v-model="lastname")
+    p {{'Nombre completo:' + fullname }}
+    label(for="dia") {{ 'Dia:' }}
+    input(v-model="dia" type="number")
+    label(for="mes") {{ 'Mes:' }}
+    input(v-model="mes" type="number")
+    label(for="año") {{ 'Año:' }}
+    input(v-model="año" type="number")
+    p {{'Tu edad es de: ' + edad + ' Años'}}
 </template>
 
 <script>
@@ -12,7 +21,21 @@ export default {
     return {
       msg: 'Hola desde vuejs!',
       name: '',
-      url: 'https://platzi.com'
+      lastname: '',
+      dia: null,
+      mes: null,
+      año: null
+    }
+  },
+  computed: {
+    fullname () {
+      return `${this.name} ${this.lastname}`
+    },
+    edad () {
+      var hoy = new Date()
+      var miFecha = new Date(this.año, this.mes, this.dia)
+      var diff = hoy.getTime() - miFecha.getTime()
+      return Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
     }
   }
 }
