87ab1f059e5da5da7ae5828f499098dc1762895e
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-23 11:19:51 -0500

    Integración a Platzi Music

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,99 +1,51 @@
 <template lang="pug">
   #app
-    h1 CURSO PROFESIONAL VUEJS
-    hr
-    h2 EXPRESIONES
-    p {{ 1 + 1 }}
-    p {{ 'Hola' + 'Mundo' }}
-    p {{ person.name }}
-    p {{ person.name.toUpperCase() }}
-    p {{ JSON.stringify(person) }}
-    p {{ true ?'true':'false'}}
-    hr
-    h2 DIRECTIVAS
-    p(v-show="showValue") {{ value }}
-    p(v-if="showValue") {{ value }}
-    p(v-else-if="false") {{ 'algo mas' }}
-    p(v-else) {{ 'lo ultimo' }}
-    ul
-      li(v-for="i in items") {{ i }}
-    hr
-    h2 DATA BINDING
-    input(v-model="name")
-    p {{ name }}
-    a(:href="url") Link
-    hr
-    h2 COMPUTED PROPERTIES
-    label(for="name") {{ 'Nombre' }}
-    input(v-model="name")
-    label(for="lastname") {{ 'Apellido' }}
-    input(v-model="lastname")
-    p {{'Nombre completo:' + fullname }}
-    label(for="dia") {{ 'Dia:' }}
-    input(v-model="dia" type="number")
-    label(for="mes") {{ 'Mes:' }}
-    input(v-model="mes" type="number")
-    label(for="año") {{ 'Año:' }}
-    input(v-model="año" type="number")
-    p {{'Tu edad es de: ' + edad + ' Años'}}
-    hr
-    h2 WATCHERS
-    label {{ 'Usar input name y ver consola navegador' }}
-    hr
-    h2 EVENTOS
-    label(for="variable") {{ 'Variable' }}
-    input(v-model="variable")
-    button(:click="format") Formatear
-    p {{ formattedVariable }}
-    hr
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="Buscar canciones", v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-dange.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
+
 </template>
 
 <script>
+const tracks = [
+  { name: 'La Chona', artist: 'Los Tucanes de Tijuana' },
+  { name: 'No rompas mas mi corazon', artist: 'Caballo Dorado' },
+  { name: 'Suavecito', artist: 'Laura Leon' },
+  { name: 'Eye of the tiger', artist: 'Quien sabe!' }
+]
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola desde vuejs!',
-      person: {
-        name: 'Andrés'
-      },
-      showValue: false,
-      value: 'Algo',
-      items: [1, 2, 3, 4, 5],
-      name: '',
-      url: 'https://platzi.com',
-      lastname: '',
-      dia: null,
-      mes: null,
-      año: null,
-      variable: '',
-      formattedVariable: ''
+      searchQuery: '',
+      tracks: []
     }
   },
   computed: {
-    fullname () {
-      return `${this.name} ${this.lastname}`
-    },
-    edad () {
-      var hoy = new Date()
-      var miFecha = new Date(this.año, this.mes, this.dia)
-      var diff = hoy.getTime() - miFecha.getTime()
-      return Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
-    }
-  },
-  watch: {
-    name (newVal, oldVal) {
-      console.log(newVal, oldVal)
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
   methods: {
-    format () {
-      this.formattedVariable = this.variable.split(' ').join('-')
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+  .results {
+    margin-top: 50px
+  }
 </style>
---
+++ platzi-music/src/Ejercicios.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+  #app
+    h1 CURSO PROFESIONAL VUEJS
+    hr
+    h2 EXPRESIONES
+    p {{ 1 + 1 }}
+    p {{ 'Hola' + 'Mundo' }}
+    p {{ person.name }}
+    p {{ person.name.toUpperCase() }}
+    p {{ JSON.stringify(person) }}
+    p {{ true ?'true':'false'}}
+    hr
+    h2 DIRECTIVAS
+    p(v-show="showValue") {{ value }}
+    p(v-if="showValue") {{ value }}
+    p(v-else-if="false") {{ 'algo mas' }}
+    p(v-else) {{ 'lo ultimo' }}
+    ul
+      li(v-for="i in items") {{ i }}
+    hr
+    h2 DATA BINDING
+    input(v-model="name")
+    p {{ name }}
+    a(:href="url") Link
+    hr
+    h2 COMPUTED PROPERTIES
+    label(for="name") {{ 'Nombre' }}
+    input(v-model="name")
+    label(for="lastname") {{ 'Apellido' }}
+    input(v-model="lastname")
+    p {{'Nombre completo:' + fullname }}
+    label(for="dia") {{ 'Dia:' }}
+    input(v-model="dia" type="number")
+    label(for="mes") {{ 'Mes:' }}
+    input(v-model="mes" type="number")
+    label(for="año") {{ 'Año:' }}
+    input(v-model="año" type="number")
+    p {{'Tu edad es de: ' + edad + ' Años'}}
+    hr
+    h2 WATCHERS
+    label {{ 'Usar input name y ver consola navegador' }}
+    hr
+    h2 EVENTOS
+    label(for="variable") {{ 'Variable' }}
+    input(v-model="variable")
+    button(:click="format") Formatear
+    p {{ formattedVariable }}
+    hr
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola desde vuejs!',
+      person: {
+        name: 'Andrés'
+      },
+      showValue: false,
+      value: 'Algo',
+      items: [1, 2, 3, 4, 5],
+      name: '',
+      url: 'https://platzi.com',
+      lastname: '',
+      dia: null,
+      mes: null,
+      año: null,
+      variable: '',
+      formattedVariable: ''
+    }
+  },
+  computed: {
+    fullname () {
+      return `${this.name} ${this.lastname}`
+    },
+    edad () {
+      var hoy = new Date()
+      var miFecha = new Date(this.año, this.mes, this.dia)
+      var diff = hoy.getTime() - miFecha.getTime()
+      return Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+    }
+  },
+  watch: {
+    name (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+  methods: {
+    format () {
+      this.formattedVariable = this.variable.split(' ').join('-')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
