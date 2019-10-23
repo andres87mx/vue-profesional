df0746a1fc1de4f7c6d547f9bde2f6aed78149d7
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-23 09:31:30 -0500

    Directivas

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
   #app
-    h1 {{ msg }}
-    p {{ 1+1}}
-    p {{ 'Hola' + 'Mundo'}}
-    p {{ person.name }}
-    p {{ person.name.toUpperCase()}}
-    p {{ JSON.stringify(person)}}
-    p {{ true ? 'true':'false'}}
+    p(v-show="showValue") {{ value }}
+    p(v-if="showValue") {{ value }}
+    p(v-else-if="false") {{ otro }}
+    p(v-else) {{ 'lo ultimo' }}
+    ul
+      li(v-for="i in items") {{ i }}
 </template>
 
 <script>
@@ -15,9 +14,10 @@ export default {
   data () {
     return {
       msg: 'Hola desde vuejs!',
-      person: {
-        name: 'Andres'
-      }
+      showValue: false,
+      value: 'Algo',
+      otro: 'Otro valor',
+      items: [1, 2, 3, 4, 5]
     }
   }
 }
