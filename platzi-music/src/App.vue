6b079c1e5ebaf1eaa4ac5b5a2ca28900a73580a1
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-28 10:16:11 -0600

    Ajuste del ejecicio

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,88 +1,51 @@
 <template lang="pug">
   #app
-    .panel.is-primary
-      .panel-heading Ejercicio de Manipulación del DOM by {{ name}}
-      .panel-block
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="Buscar canciones", v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-dange.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
         .columns
-          .column
-            .box
-              .field
-                  p.is-size-3 New Task
-              .field
-                label.control Title:
-                input.control(type="text", placeholder="Title", v-model="title")
-              .field
-                label.control Time
-                input.control(type="numeric", placeholder="Time", v-model="time")
-              .field
-                button.button.is-info(@click="addTask") Add Task
-                button.button.is-danger(@click="resetValues") Cancel
-          .column
-              .fieldset(v-if="tasks.length>0")
-                p.is-size-5(v-show="totalTime") Horas trabajadas: {{ totalTime }}
-                .columns
-                  .column(v-for="(t, key) in tasks")
-                    .box
-                      p.is-size-7.has-text-weight-bold {{ t.title }}
-                      p.is-size-7 {{ t.time}}
-                      button.button.is-small(@click="removeTask(key)") Delete
-              .fieldset(v-else)
-                h1 No tasks yet!
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
+
 </template>
 
 <script>
+const tracks = [
+  { name: 'La Chona', artist: 'Los Tucanes de Tijuana' },
+  { name: 'No rompas mas mi corazon', artist: 'Caballo Dorado' },
+  { name: 'Suavecito', artist: 'Laura Leon' },
+  { name: 'Eye of the tiger', artist: 'Quien sabe!' }
+]
 export default {
   name: 'app',
   data () {
     return {
-      name: 'ANDRES RUIZ',
-      tasks: [],
-      newTask: {},
-      title: '',
-      time: 0,
-      existsTasks: false
+      searchQuery: '',
+      tracks: []
     }
   },
   computed: {
-    totalTime () {
-      var time = 0
-      for (let index = 0; index < this.tasks.length; index++) {
-        time += parseInt(this.tasks[index].time)
-      }
-      return time
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
-  created () {
-    this.tasks = JSON.parse(localStorage.getItem('tasks')) || []
-  },
   methods: {
-    addTask () {
-      if ((this.title !== '') && (this.time > 0)) {
-        this.tasks.push({
-          title: this.title,
-          time: this.time
-        })
-        alert('Added correctly!')
-        this.resetValues()
-        localStorage.setItem('tasks', JSON.stringify(this.tasks))
-      } else {
-        alert('Check your values!')
-      }
-    },
-    resetValues () {
-      this.title = ''
-      this.time = 0
-      this.newTask = {}
-    },
-    removeTask (key) {
-      this.tasks.splice(key, 1)
-      alert('Deleted correctly!')
-      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+  .results {
+    margin-top: 50px
+  }
 </style>
--- platzi-music/src/Ejemplo.vue
+++ platzi-music/src/Ejemplo.vue
@@ -1,11 +1,33 @@
 <template lang="pug">
   #app
-    section.section
-      .container
-        label {{ name }}
-      .container
-        label tasks
-    
+    .panel.is-primary
+      .panel-heading Ejercicio de Manipulación del DOM by {{ name}}
+      .panel-block
+        .columns
+          .column
+            .box
+              .field
+                  p.is-size-3 New Task
+              .field
+                label.control Title:
+                input.control(type="text", placeholder="Title", v-model="title")
+              .field
+                label.control Time
+                input.control(type="numeric", placeholder="Time", v-model="time")
+              .field
+                button.button.is-info(@click="addTask") Add Task
+                button.button.is-danger(@click="resetValues") Cancel
+          .column
+              .fieldset(v-if="tasks.length>0")
+                p.is-size-5(v-show="totalTime") Horas trabajadas: {{ totalTime }}
+                .columns
+                  .column(v-for="(t, key) in tasks")
+                    .box
+                      p.is-size-7.has-text-weight-bold {{ t.title }}
+                      p.is-size-7 {{ t.time}}
+                      button.button.is-small(@click="removeTask(key)") Delete
+              .fieldset(v-else)
+                h1 No tasks yet!
 </template>
 
 <script>
@@ -13,15 +35,50 @@ export default {
   name: 'app',
   data () {
     return {
-      name: '',
-      task: [1, 2, 3, 4, 5]
+      name: 'ANDRES RUIZ',
+      tasks: [],
+      newTask: {},
+      title: '',
+      time: 0,
+      existsTasks: false
     }
   },
   computed: {
+    totalTime () {
+      var time = 0
+      for (let index = 0; index < this.tasks.length; index++) {
+        time += parseInt(this.tasks[index].time)
+      }
+      return time
+    }
   },
-  watch: {
+  created () {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || []
   },
   methods: {
+    addTask () {
+      if ((this.title !== '') && (this.time > 0)) {
+        this.tasks.push({
+          title: this.title,
+          time: this.time
+        })
+        alert('Added correctly!')
+        this.resetValues()
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      } else {
+        alert('Check your values!')
+      }
+    },
+    resetValues () {
+      this.title = ''
+      this.time = 0
+      this.newTask = {}
+    },
+    removeTask (key) {
+      this.tasks.splice(key, 1)
+      alert('Deleted correctly!')
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    }
   }
 }
 </script>
