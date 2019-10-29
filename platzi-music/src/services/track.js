15c57ff4a6011b814ef4fbf73db76b40f136d79b
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-29 10:54:32 -0600

    Fetch API & Trae

---
+++ platzi-music/src/services/track.js
@@ -0,0 +1,11 @@
+import platziMusicService from './platziMusicService'
+const trackService = {}
+
+trackService.search = function (q) {
+    const type = 'track'
+    return platziMusicService.get('/search', {
+        params: { q, type}
+    })
+    .then(response => response.data)
+}
+export default trackService
