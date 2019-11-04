03cc36033109a5e83b7ddb76f000881a18f9fe76
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-11-04 11:26:55 -0600

    vue-router

--- platzi-music/src/components/Player.vue
+++ platzi-music/src/components/Player.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
-    .content
+    .content(v-if="track && track.album")
       p.is-128x128
         img(:src="track.album.images[0].url")
       p
         strong {{ track.name }}
         small [{{ track.duration_ms }}]
-      p
+      p(v-if="track.preview_url")
         audio(controls, :src="track.preview_url")
+      p(v-else)
+        strong Sin preview de cancion.
 </template>
 <script>
 export default {
---
+++ platzi-music/src/components/Search.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+  #main
+    pm-notification(v-show="showNotification", :notification="notify")
+      //- p(slot="body") No se encontraron resultados!
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.nav
+        .container
+          input.input.is-large(type="text", placeholder="Buscar canciones", v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-dange.is-large &times;
+      .container
+        p
+          small {{ searchMessage }}
+      .container.results
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(:track="t", v-on:select="setSelectedTrack", v-bind:class="{ 'is-active': t.id == selectedTrack }")
+</template>
+
+<script>
+import trackService from '@/services/track'
+
+import PmTrack from '@/components/Track.vue'
+
+import PmLoader from '@/components/shared/Loader.vue'
+import PmNotification from '@/components/shared/Notification.vue'
+
+export default {
+  name: 'app',
+  components: { PmTrack, PmLoader, PmNotification },
+  data () {
+    return {
+      searchQuery: '',
+      tracks: [],
+      isLoading: false,
+      selectedTrack: '',
+      showNotification: false,
+      notify: {}
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
+  methods: {
+    search () {
+      if (!this.searchQuery) { return }
+      this.isLoading = true
+      trackService.search(this.searchQuery)
+        .then(
+          res => {
+            if (res.tracks.total === 0) {
+              this.notify = {
+                message: 'Algo anduvo mal',
+                type: 'is-danger'
+              }
+            } else {
+              this.notify = {
+                message: 'Se encontraron resultados',
+                type: 'is-success'
+              }
+            }
+            this.showNotification = true
+            this.tracks = res.tracks.items
+            this.isLoading = false
+          })
+    },
+    setSelectedTrack (id) {
+      this.selectedTrack = id
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .results {
+    margin-top: 50px;
+  }
+  .is-active {
+    border: 3px #23d160 solid;
+  }
+</style>
