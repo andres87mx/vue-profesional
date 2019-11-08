f270999c09959eed8fa94bb524dec810f4a2ea52
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-11-08 12:40:56 -0600

    Nuxt at 2

---
+++ platzi-music-nuxt/components/Header.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    section.hero.is-success
+        .hero-head
+            header.nav
+                .container
+                  .navbar
+                    .navbar-brand
+                      .navbar-item
+                        strong Platzi Music 🎵
+                    .navbar-end
+                      .navbar-item
+                        router-link.nav-item(to="search") Buscar
+                      .navbar-item
+                        router-link.nav-item(to="about") Nosotros
+        .hero-body
+            .container.has-text-centered
+                h1.title Platzi Music
+                h2.subtitle Canciones que estan buenisimas
+</template>
---
+++ platzi-music-nuxt/plugins/track.js
@@ -0,0 +1,13 @@
+import platziMusicService from './platzi-music'
+const trackService = {}
+trackService.search = function (q) {
+  const type = 'track'
+  return platziMusicService.get('/search', {
+    params: { q, type }
+  })
+    .then(response => response.data)
+}
+trackService.getById = function (id) {
+  return platziMusicService.get(`/tracks/${id}`).then(res => res.data)
+}
+export default trackService
